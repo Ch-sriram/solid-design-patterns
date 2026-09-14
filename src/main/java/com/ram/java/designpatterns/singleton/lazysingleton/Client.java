1e4b7373b54d304f27e380995bc096df934c5a0d
Ch-sriram <sriram.ch1995@gmail.com>
2026-09-14 12:06:11 +0530

    Add `Client.java` for Lazy Singleton Pattern

---
+++ src/main/java/com/ram/java/designpatterns/singleton/lazysingleton/Client.java
@@ -0,0 +1,20 @@
+package com.ram.java.designpatterns.singleton.lazysingleton;
+
+/**
+ * Demonstrates the usage of {@link LazyRegistryWithDoubleNullCheckLocking}
+ * object, which is also a demonstration of Lazy Singleton Pattern.
+ */
+public class Client {
+
+	public static void main(String[] args) {
+		// Creation of the object by any Client isn't possible, as can be seen below:
+		// LazyRegistryWithDoubleNullCheckLocking registry = new LazyRegistryWithDoubleNullCheckLocking();
+
+		LazyRegistryWithDoubleNullCheckLocking registry = LazyRegistryWithDoubleNullCheckLocking.getInstance();
+		LazyRegistryWithDoubleNullCheckLocking registry2 = LazyRegistryWithDoubleNullCheckLocking.getInstance();
+
+		// Both registry and registry2 objects are referencing the same object in memory.
+		System.out.print(registry == registry2);	// O/P: true
+	}
+
+}
